e5866696daa5798027de4e2a18ee0f955f1f57c3
RAbnza <rendelabainza11@gmail.com>
2025-08-14 18:38:52 +0800

    fix: adjust layout component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,16 +8,19 @@
 
         <link rel="icon" href="/images/radmedics-logo.png" type="image/png">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Tailwind CSS -->
+    @vite('resources/css/app.css')
 
     </head>
-    <body style="font-family: 'Poppins', sans-serif !important;" class="bg-gray-50 min-h-screen">
+    <body class="bg-gray-50 min-h-screen font-[Poppins]">
         {{-- TODO: Add header component --}}
 
-        <main class="max-2xl:block hidden px-4 py-6">
+    <main class="max-2xl:block px-4 py-6 font-[Poppins]">
             {{ $slot }}
         </main>
 
