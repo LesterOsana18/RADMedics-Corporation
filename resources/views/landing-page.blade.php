970e81d9215e9386ff425474d580bc3f4e4367d1
RM <122985479+rmudw@users.noreply.github.com>
2025-08-20 02:00:27 +0800

    feat: add initial landing page structure

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,4 +1,23 @@
-<x-layout>
-    <!-- Landing page intentionally left blank to show header/navbar only -->
-    <!-- Currently working on the footer component -->
+<x-layout style="margin-top: 50px;">
+    <div id="welcome-container" class="pt-[7rem] text-center">
+        <span class="bg-teal-800 text-white px-6 py-2 rounded-full font-semibold">
+            RADMedics Corporation
+        </span>
+        <h1 class="mt-6 text-8xl font-extrabold text-teal-500">
+            Response Advocates <br>
+            for Development of Medics
+        </h1>
+        <p class="mt-4 text-teal-800 max-w-2xl mx-auto">
+            An initiative dedicated to empowering and advancing healthcare professionals 
+            through technology, training, and innovation.
+        </p>
+        <button class="px-5 py-2 bg-white text-teal-800 border border-teal-800 hover:bg-teal-600 hover:text-white rounded-full font-medium  inline-flex items-center space-x-2">
+            <span>Learn More</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
+
+        <!-- <x-layouts.button type="outline" color="tiffany-blue" size="sm" text="Learn More"></x-layoyuts.button> -->
+    </div>
 </x-layout>
