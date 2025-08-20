98707531d1c6cdda586f075013b57c8d24fb41cd
RM <122985479+rmudw@users.noreply.github.com>
2025-08-20 23:08:07 +0800

    fix: adjusted top margin of landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="pt-[13.5rem] text-center">
+    <div class="pt-[10.5rem] text-center">
         <span class="bg-teal-800 text-white text-2xl px-16 py-2 rounded-full font-bold">
             RADMedics Corporation
         </span>
@@ -10,7 +10,7 @@
         <p class="mt-8 text-teal-800 text-xl tracking-widest mx-5">
             An initiative dedicated to empowering and advancing healthcare professionals through technology, training, and innovation.
         </p>
-        <button class="mt-9 mb-[5rem] px-5 py-2 bg-white text-teal-800 border-2 border-teal-800 hover:bg-teal-600 hover:text-white rounded-full font-semibold  inline-flex items-center space-x-2">
+        <button class="mt-9 px-5 py-2 bg-white text-teal-800 border-2 border-teal-800 hover:bg-teal-600 hover:text-white rounded-full font-semibold  inline-flex items-center space-x-2">
             <span>Learn More</span>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
