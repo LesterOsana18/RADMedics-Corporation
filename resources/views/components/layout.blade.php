42bc8e65144a13a1b8d033fcb999eda3ff9a3ede
LesterOsana18 <lesterfosana@iskolarngbayan.pup.edu.ph>
2025-08-24 00:22:14 +0800

    feat: update Poppins font weights for improved typography consistency

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <!-- Fonts: Only Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Tailwind CDN with Poppins font only -->
     <script src="https://cdn.tailwindcss.com"></script>
