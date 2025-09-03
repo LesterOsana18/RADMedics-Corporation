6eb74fb1c498372411035f8ea5c897b8077b3edc
RAbnza <abainzarendel11@gmail.com>
2025-09-04 02:19:35 +0800

    feat: add Docker configuration and deployment scripts for Laravel application

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "Running composer"
+
+
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+
+
+echo "Publishing cloudinary provider..."
+php artisan vendor:publish --provider="Cloudinary\CloudinaryLaravel\CloudinaryServiceProvider --tag=cloudinary-laravel-config"
+
+
